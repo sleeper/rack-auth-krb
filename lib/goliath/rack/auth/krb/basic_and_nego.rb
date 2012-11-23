2c23964445806e01cb3ed6a9df5983a75d786e61
mbridard <michael.bridard@amadeus.com>
2012-11-23 16:25:14 +0000

    Add the possibility to enable authentication process for a set of paths only

--- lib/goliath/rack/auth/krb/basic_and_nego.rb
+++ lib/goliath/rack/auth/krb/basic_and_nego.rb
@@ -1,7 +1,7 @@
 require 'goliath'
 require 'basic_and_nego/request'
 require 'basic_and_nego/processor'
-
+require 'ruby-debug'
 module Goliath
   module Rack
     module Auth
@@ -9,20 +9,27 @@ module Goliath
         class BasicAndNego
           include Goliath::Rack::AsyncMiddleware
 
-          def initialize(app, realm, keytab, service=nil)
+          def initialize(app, realm, keytab, service=nil, paths_only=Array.new)
             @app = app
             @realm = realm
             @keytab = keytab
             @service = service
+			#paths_only allows to request an authentication process only for specified paths
+			@paths_only = paths_only 			
           end
 
           def call(env)
-            a = ::BasicAndNego::Processor.new(env, env.logger, @realm, @keytab, @service)
-            a.process_request
+			a = nil
+			
+			if @paths_only.empty? or @paths_only.include?(env["PATH_INFO"])
+				a = ::BasicAndNego::Processor.new(env, env.logger, @realm, @keytab, @service)
+				a.process_request
 
-            return a.response if a.response
-
-            super(env, a.headers)
+				return a.response if a.response
+			end
+			new_headers = (a.nil?) ? {} : a.headers
+			
+            super(env, new_headers)
           end
 
           def post_process(env, status, headers, body, additional_headers)
--- lib/rack/auth/krb/basic_and_nego.rb
+++ lib/rack/auth/krb/basic_and_nego.rb
@@ -6,26 +6,35 @@ module Rack
     module Krb
       class BasicAndNego
 
-        def initialize(app, realm, keytab, service=nil)
+        def initialize(app, realm, keytab, service=nil, paths_only=Array.new)
           @app = app
           @realm = realm
           @keytab = keytab
           @service = service
+          #paths_only allows to request an authentication process only for specified paths
+          @paths_only = paths_only 
         end
 
         def call(env)
-          # Either user rack.logger if defined or create on
-          # logger defaulting to rack.errors
-          #
-          logger = env['rack.logger'] || ::Logger.new(env['rack.errors'])
-          a = ::BasicAndNego::Processor.new(env, logger, @realm, @keytab, @service)
-          a.process_request
 
-          return a.response if a.response
-
+          a = nil
+          
+          if @paths_only.empty? or @paths_only.include?(env["PATH_INFO"])
+			# Either user rack.logger if defined or create on
+			# logger defaulting to rack.errors		  
+            logger = env['rack.logger'] || ::Logger.new(env['rack.errors'])
+            a = ::BasicAndNego::Processor.new(env, logger, @realm, @keytab, @service)
+            a.process_request
+            return a.response if a.response
+          end
+          
           status, headers, body = @app.call(env)
-
-          [status, headers.merge(a.headers), body]
+          
+          if a
+            headers.merge!(a.headers)
+          end
+          
+          [status, headers, body]
         end
       end
     end
