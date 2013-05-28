1ae61d08443cc29f596791e930959132bb51b133
Bridou <mickael.bridard@gmail.com>
2013-05-28 12:01:05 +0000

    Try to add the realm as suffix while doing a basic authent and then without it

--- spec/basic_and_nego/auth/basic_spec.rb
+++ spec/basic_and_nego/auth/basic_spec.rb
@@ -22,10 +22,11 @@ describe BasicAndNego::Auth::Basic do
   it "should try authentication against Kerberos in case of Basic" do
     @krb.should_receive(:authenticate).with("fred", "pass").and_return(true)
     @a.process
+    @a.client_name.should == "fred"
   end
 
-  it "should return 'unauthorized' if authentication fails" do 
-    @krb.should_receive(:authenticate).and_return(false)
+  it "should return 'unauthorized' if authentication fails" do
+    @krb.should_receive(:authenticate).and_return(false, false)
     @a.process
     @a.response.should_not be_nil
     @a.response[0].should == 401
@@ -42,5 +43,35 @@ describe BasicAndNego::Auth::Basic do
     @a.process
     @a.client_name.should == "fred"
   end
+  
+  it "should try authentication against Kerberos in case of Basic adding automatically the realm" do
+    @krb.should_receive(:authenticate).and_return(true)
+    @a.process
+    @a.client_name.should == "fred"
+  end  
 
+ 
+end
+
+describe "BasicAndNego::Auth::Basic with specific realm" do
+  
+  before(:each) do 
+    env = {'HTTP_AUTHORIZATION' => "Basic #{::Base64.encode64('fred@customRealm:pass')}"}
+    @realm = "my realm"
+    @keytab = "my keytab"
+    @service = "http/hostname"
+    @logger = BasicAndNego::NullLogger.new
+    @request = BasicAndNego::Request.new(env)
+    @request.should_receive(:credentials).and_return(['fred@customRealm', 'pass'])
+    @krb = double('kerberos').as_null_object
+    BasicAndNego::Auth::Krb.should_receive(:new).with(@logger, @realm, @keytab).and_return(@krb)
+    @a = BasicAndNego::Auth::Basic.new(@request, @logger, @realm, @keytab, @service)
+  end  
+  
+  it "should try authentication against Kerberos in case of Basic" do
+    @krb.should_receive(:authenticate).with("fred@customRealm", "pass").and_return(true)
+    @a.process
+    @a.client_name.should == "fred@customRealm"
+  end
+    
 end
